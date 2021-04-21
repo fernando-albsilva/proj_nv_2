5a64b5a386e8f57ed11196c1ef409303ba5b0710
Fernando Albuquerque Silva <49257064+fernando-albsilva@users.noreply.github.com>
2021-04-21 15:32:05 -0300

    upload arquivos

---
+++ python_flask/app.py
@@ -0,0 +1,16 @@
+from pacienteController import PacienteController
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/index')
+def responde():
+    pacienteInstancia = PacienteController()
+    lista = pacienteInstancia.retornaJson()
+    return str(lista)
+
+app.run()
+
+
+
---
+++ python_flask/paciente.py
@@ -0,0 +1,13 @@
+class Paciente:
+
+    nome:str   
+    email:str
+    telefone:str
+    endereco:str
+ 
+    def __init__(self):
+        self.nome="j"
+        self.email="e"
+
+    
+    
